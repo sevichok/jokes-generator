19d07b597b3ee8a555864a4c598981b7824d8cc7
sevichok <sevichok41294@gmail.com>
2022-10-25 18:55:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [joke, setJoke] = useState(null)
+
+  const getJoke = async () => {
+    const jokeData = await fetch(`https://icanhazdadjoke.com/`,
+      {
+        headers: { Accept: "application/json" },
+      }
+    )
+    const jokeObj = await jokeData.json()
+    setJoke(jokeObj)
+  };
+
+  return (
+    <div className="App">
+      <h2>Random Joke Generator</h2>
+      <div className="joke_area">
+        {joke && <h4>{joke.joke}</h4>}
+        <button className='joke_button' onClick={() => getJoke()}>Get a New Joke</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
